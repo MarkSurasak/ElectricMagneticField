8425acb7f1ee45bdad1788c6467120734440e793
MarkSurasak <61034796+MarkSurasak@users.noreply.github.com>
2022-06-24 20:20:04 +0700

    Update Solenoid.js

--- src/curve/Solenoid.js
+++ src/curve/Solenoid.js
@@ -1,7 +1,22 @@
-import { Curve } from "three";
+import { Curve, Vector3} from "three";
 
 class SoleniodCurve extends Curve {
     
+    constructer(period, length, radius) {
+        super();
+        
+        this.period = period;
+        this.length = lenght;
+        this.radius = redius;
+    }
+    
+    getPoint(t, optionalTarget = new Vector3()) {
+    	    if !(0 <= t <= 1) {
+    	    	console.error("t value must between 0 and 1");
+    	    }
+    	    
+    	    return optionalTarget.set(this.radius*Math.sin(this.period*2*Math.pi*t,t*this.length, this.radius*Math.cos(this.period*2*Math.pi*t)
+    }
 }
 
 export { SoleniodCurve };
