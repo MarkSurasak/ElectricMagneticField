88c09b79d73c9d32aad7b23d4e1e419e8089e383
MarkSurasak <61034796+MarkSurasak@users.noreply.github.com>
2022-07-09 22:29:27 +0700

    Update ParametricCurve.js

--- src/curve/ParametricCurve.js
+++ src/curve/ParametricCurve.js
@@ -7,14 +7,28 @@ class ParametricCurve extends Curve {
     this.type = "parametric";
   }
 
-  getTangents(divisions = 5) {
-    const tangents = [];
+  getTangent(t, optionalTaget = new Vector3()) {
+    return optionalTaget.copy(this.getDerivative(t)).normalize()
+  }
 
-    for (let d = 0; d <= divisions; d++) {
-      tangents.push(this.getTangent(d / divisions));
-    }
+  getDerivative(t, optionalTaget = new Vector3()) {
+    const delta = 0.0001;
+    let t1 = t - delta
+    let t2 = t + delta;
 
-    return tangents;
+    // Capping in case of danger
+
+    if ( t1 < 0 ) t1 = 0;
+    if ( t2 > 1 ) t2 = 1;
+ 
+    const pt1 = this.getPoint( t1 );
+    const pt2 = this.getPoint( t2 );
+
+    const derivative = optionalTarget || ( ( pt1.isVector2 ) ? new Vector2() : new Vector3() );
+
+    derivative.copy( pt2 ).sub( pt1 );
+
+    return derivative
   }
 }
 
